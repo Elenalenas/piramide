bf549a8b717c513a189338ddd7acd72169d4cc33
ElenaMelnychenko <31886646+Elenalenas@users.noreply.github.com>
2018-01-15 15:45:25 +0200

    Add files via upload

---
+++ piramide.js
@@ -0,0 +1,13 @@
+var str = '';
+for (var i = 1; i <= 9; i++){
+for (var j = 1; j <= i; j++){
+document.write(i)
+}
+document.write('<br>')
+}
+
+var str = '';
+for (var i = 0; i <= 10; i++){
+str += 'xx';
+document.write(str + '<br>')
+}
